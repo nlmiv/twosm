6345f910ceeb5efc09e504f161def6d7da23f515
nlmiv <napmanaog@aim.com>
2021-07-28 01:05:53 +1000

    listing crud

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,13 +1,39 @@
 class ListingsController < ApplicationController
   def index
+    @listings = Listing.all
   end
 
   def new
+    @listing = Listing.new
+  end
+
+  def create
+    listing = Listing.create listing_params
+    redirect_to listing
   end
 
   def edit
+    @listing = Listing.find params[:id]
+  end
+
+  def update
+    listing = Listing.find params[:id]
+    listing.update listing_params
+    redirect_to listing # GET show
   end
 
   def show
+    @listing = Listing.find params[:id]
+  end
+
+  def destroy
+    listing = Listing.find params[:id]
+    listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:name, :description, :price, :image)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Listing.destroy_all
+l1 = Listing.create :name => 'Item 1', :description => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, eligendi!', :price => 10.00
+l2 = Listing.create :name => 'Item 2', :description => 'Quis, eligendi!, consectetur adipisicing elit. Dolor sit amet', :price => 99.99
+l3 = Listing.create :name => 'Item 3', :description => 'Quis, eligendi!, consectetur adipisicing elit. Dolor sit amet', :price => 88.88
+l4 = Listing.create :name => 'Item 4', :description => 'Quis, eligendi!, consectetur adipisicing elit. Dolor sit amet', :price => 77.50
+puts "#{ Listing.count } listings"
